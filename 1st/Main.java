685c63ca79681c9eb0aa5110a331ab40c2e06bac
bkjames <sweden011@gmail.com>
2018-12-02 08:09:02 +0900

    Update Main.java

--- 1st/Main.java
+++ 1st/Main.java
@@ -5,13 +5,13 @@ package block01;
 import java.util.ArrayList;
 import com.google.gson.GsonBuilder;
 
-public class CwChain {
+public class Main {
 	
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 	public static int difficulty = 3;
 
 	public static void main(String[] args) {	
-		//add our blocks to the blockchain ArrayList:
+	
 		
 		System.out.println("Mining block 1... ");
 		addBlock(new Block("first block", "0"));
@@ -34,21 +34,21 @@ public class CwChain {
 		Block previousBlock;
 		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 		
-		//loop through blockchain to check hashes:
+		
 		for(int i=1; i < blockchain.size(); i++) {
 			currentBlock = blockchain.get(i);
 			previousBlock = blockchain.get(i-1);
-			//compare registered hash and calculated hash:
+		
 			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
 				System.out.println("Current Hashes not equal");			
 				return false;
 			}
-			//compare previous hash and registered previous hash
+	
 			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
 				System.out.println("Previous Hashes not equal");
 				return false;
 			}
-			//check if hash is solved
+		
 			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
 				System.out.println("This block hasn't been mined");
 				return false;

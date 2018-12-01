ec0adc7a9f6549a04674995be2cf1d358d6433db
bkjames <sweden011@gmail.com>
2018-12-02 08:07:07 +0900

    Update and rename StringUtil.java to BlockUtil.java

--- 1st/BlockUtil.java
+++ 1st/BlockUtil.java
@@ -1,20 +1,20 @@
-package cwchain;
-import java.security.MessageDigest;
+package block01;
 
+
+import java.security.MessageDigest;
 import com.google.gson.GsonBuilder;
 
-public class StringUtil {
+public class BlockUtil {
 	
-	//Applies Sha256 to a string and returns the result. 
+
 	public static String applySha256(String input){
 		
 		try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
 	        
-			//Applies sha256 to our input, 
 			byte[] hash = digest.digest(input.getBytes("UTF-8"));
 	        
-			StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
+			StringBuffer hexString = new StringBuffer(); 
 			for (int i = 0; i < hash.length; i++) {
 				String hex = Integer.toHexString(0xff & hash[i]);
 				if(hex.length() == 1) hexString.append('0');
@@ -27,12 +27,12 @@ public class StringUtil {
 		}
 	}
 	
-	//Short hand helper to turn Object into a json string
+
 	public static String getJson(Object o) {
 		return new GsonBuilder().setPrettyPrinting().create().toJson(o);
 	}
 	
-	//Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"  
+
 	public static String getDificultyString(int difficulty) {
 		return new String(new char[difficulty]).replace('\0', '0');
 	}

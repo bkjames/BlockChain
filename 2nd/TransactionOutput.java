3f394fa9443bf9a5635df94d522c6e29d6993af2
bkjames <sweden011@gmail.com>
2019-02-19 20:57:03 +0900

    Update TransactionOutput.java

--- 2nd/TransactionOutput.java
+++ 2nd/TransactionOutput.java
@@ -1,24 +1,22 @@
-package cwchain;
+package BlockChain02;
 
 import java.security.PublicKey;
 
 public class TransactionOutput {
+
 	public String id;
-	public PublicKey reciepient; //also known as the new owner of these coins.
-	public float value; //the amount of coins they own
-	public String parentTransactionId; //the id of the transaction this output was created in
+	public PublicKey reciepient;
+	public float value;
+	public String parentTransactionId;
 	
-	//Constructor
 	public TransactionOutput(PublicKey reciepient, float value, String parentTransactionId) {
 		this.reciepient = reciepient;
 		this.value = value;
 		this.parentTransactionId = parentTransactionId;
-		this.id = StringUtil.applySha256(StringUtil.getStringFromKey(reciepient)+Float.toString(value)+parentTransactionId);
+		this.id = BlockUtil.applySha256(BlockUtil.getStringFromKey(reciepient)+Float.toString(value)+parentTransactionId);
 	}
 	
-	//Check if coin belongs to you
 	public boolean isMine(PublicKey publicKey) {
 		return (publicKey == reciepient);
 	}
-	
 }

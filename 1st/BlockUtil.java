34945cb314e487b38d8d4e3effb920d310f1f044
bkjames <sweden011@gmail.com>
2019-02-21 16:22:15 +0900

    Update BlockUtil.java

--- 1st/BlockUtil.java
+++ 1st/BlockUtil.java
@@ -33,7 +33,7 @@ public class BlockUtil {
 	}
 	
 
-	public static String getDificultyString(int difficulty) {
+	public static String getDifficultyString(int difficulty) {
 		return new String(new char[difficulty]).replace('\0', '0');
 	}
 	

373c605d9cb40f74c591d2731c865206e5d2a97f
bkjames <sweden011@gmail.com>
2019-03-01 12:10:52 +0900

    Update TransactionOutput.java

--- 2nd/TransactionOutput.java
+++ 2nd/TransactionOutput.java
@@ -1,14 +1,14 @@
-package BlockChain02;
+package Part2;
 
 import java.security.PublicKey;
 
 public class TransactionOutput {
-
 	public String id;
-	public PublicKey reciepient;
-	public float value;
-	public String parentTransactionId;
+	public PublicKey reciepient; //받는사람
+	public float value; //금액
+	public String parentTransactionId; 
 	
+	//생성자
 	public TransactionOutput(PublicKey reciepient, float value, String parentTransactionId) {
 		this.reciepient = reciepient;
 		this.value = value;
@@ -16,7 +16,9 @@ public class TransactionOutput {
 		this.id = BlockUtil.applySha256(BlockUtil.getStringFromKey(reciepient)+Float.toString(value)+parentTransactionId);
 	}
 	
+	//받는 사람 주소가 == 그 사람의 계좌주소가 같다면 동일인
 	public boolean isMine(PublicKey publicKey) {
 		return (publicKey == reciepient);
 	}
+	
 }

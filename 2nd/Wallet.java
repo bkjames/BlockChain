93be63a8744b2fb4ce7b96cce6cb0c123bb23672
bkjames <sweden011@gmail.com>
2019-03-01 12:09:34 +0900

    Update Wallet.java

--- 2nd/Wallet.java
+++ 2nd/Wallet.java
@@ -1,82 +1,75 @@
-package BlockChain02;
-
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.SecureRandom;
+package Part2;
+import java.security.*;
 import java.security.spec.ECGenParameterSpec;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Wallet {
-
 	
 	public PrivateKey privateKey;
 	public PublicKey publicKey;
 	
-	public HashMap<String , TransactionOutput> UTXO_Wallet = new HashMap<>();
+	public HashMap<String,TransactionOutput> UTXO_Wallet = new HashMap<String,TransactionOutput>();
 	
 	public Wallet() {
 		generateKeyPair();
 	}
-	
-	
+	// 잔고 확인 기능
 	public float getBalance() {
-		float total =0;
-		for(Map.Entry<String, TransactionOutput> item : Main.UTXOs.entrySet()) {
-			TransactionOutput UTXO = item.getValue();
-			if(UTXO.isMine(publicKey)) {
-				UTXO_Wallet.put(UTXO.id, UTXO);
-				total += UTXO.value;
-			}
-		}
+		float total = 0;	
+        for (Map.Entry<String, TransactionOutput> item: Main.UTXOs.entrySet()){
+        	TransactionOutput UTXO = item.getValue();
+            if(UTXO.isMine(publicKey)) { 
+            	UTXO_Wallet.put(UTXO.id,UTXO); 
+            	total += UTXO.value ; 
+            }
+        }  
 		return total;
 	}
-	
-	public Transaction sendFunds(PublicKey _reciepient, float value) {
-		if(getBalance()< value) {
-			System.out.println("Not Enough Money");
+	// 송신기능
+	public Transaction sendFunds(PublicKey _recipient,float value ) {
+		if(getBalance() < value) {
+			System.out.println("Not Enough~~!");
 			return null;
 		}
+		ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
 		
-		ArrayList<TransactionInput> inputs  = new ArrayList<>();
-		
-		float total =0;
-		for(Map.Entry<String, TransactionOutput> item : UTXO_Wallet.entrySet()) {
+		float total = 0;
+		for (Map.Entry<String, TransactionOutput> item: UTXO_Wallet.entrySet()){
 			TransactionOutput UTXO = item.getValue();
 			total += UTXO.value;
 			inputs.add(new TransactionInput(UTXO.id));
-			if(total>value) break;
+			if(total > value) break;
 		}
 		
-		Transaction newTransaction = new Transaction(publicKey, _reciepient, value, inputs);
+		Transaction newTransaction = new Transaction(publicKey, _recipient , value, inputs);
 		newTransaction.generateSignature(privateKey);
 		
-		for(TransactionInput input : inputs) {
+		for(TransactionInput input: inputs){
 			UTXO_Wallet.remove(input.transactionOutputId);
 		}
+		
 		return newTransaction;
 	}
-
+	
 	public void generateKeyPair() {
-			try {
-				KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
-				SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-				ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
-			
-				keyGen.initialize(ecSpec, random); 
-		        KeyPair keyPair = keyGen.generateKeyPair();
-		     
-		        privateKey = keyPair.getPrivate();
-		        publicKey = keyPair.getPublic();
-		        
-			}catch(Exception e) {
-				throw new RuntimeException(e);
-			}
+		try {
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+	
+			keyGen.initialize(ecSpec, random); 
+	        KeyPair keyPair = keyGen.generateKeyPair();
+	
+	        privateKey = keyPair.getPrivate();
+	        publicKey = keyPair.getPublic();
+	        
+		}catch(Exception e) {
+			throw new RuntimeException(e);
 		}
-	
-	
+	}
 	
 }
+
+
